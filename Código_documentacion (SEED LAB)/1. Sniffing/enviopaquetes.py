abecd542d85233627f92fae5509977eec3b552d9
DavidJurado98 <hdjp1998@gmail.com>
2019-04-08 12:13:31 +0200

    CODIGO PROYECTO

--- Código_documentacion (SEED LAB)/1. Sniffing/enviopaquetes.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import logging
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-from scapy.all import *
-a = IP()
-a.dst = '192.168.50.50'
-b = ICMP()
-p = a/b
-send(p)
--- Código_documentacion (SEED LAB)/1. Sniffing/sniffer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# La ruta para ipv6 no es necesaria para  este programa 
-# entonces ponemos esto para que no salte el WARNING
-from scapy.all import *
-def print_pkt(pkt):
-  pkt.show()
-
-pkt = sniff(filter='icmp',prn=print_pkt)
