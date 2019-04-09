bdbbbe8b1176502a6ae66fcfc3008189dac31c09
DavidJurado98 <33413477+DavidJurado98@users.noreply.github.com>
2019-04-09 16:02:26 +0200

    CODIGO PROYECTO

--- Código_documentacion (SEED LAB)/1. Sniffing/1.1 A&B Sniffing Packets/sniffer.py
+++ Código_documentacion (SEED LAB)/1. Sniffing/1.1 A&B Sniffing Packets/sniffer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-# LINEA 4 y 5 SOLO SI SALTA EL ERROR DEL IPV6
+# LINEA 5 y 6 SOLO SI SALTA EL ERROR DEL IPV6
 # La direccion para ipv6 no es necesaria para  este programa 
-# entonces añadimos la linea 4 y 5 que elimina las advertencias para que no salte el WARNING
+# entonces añadimos la linea 5 y 6 que elimina las advertencias para que no salte el WARNING
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
