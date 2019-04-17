96fe460c8a389c009e123714c8c55adaa63cd34a
DavidJurado98 <33413477+DavidJurado98@users.noreply.github.com>
2019-04-17 20:30:59 +0200

    CODIGO PROYECTO

--- Código_documentacion (SEED LAB)/1. Sniffing/1.3 Traceroute/traceroute.py
+++ Código_documentacion (SEED LAB)/1. Sniffing/1.3 Traceroute/traceroute.py
@@ -3,13 +3,15 @@ from scapy.all import *
 
 print "=======================[TRACEROUTE]======================="
 #Introducimos la direccion:
-direccion = "www.google.com"
+direccion = "www.iana.org"
 for ttl in range(1, 28):
     pkt = IP(dst=direccion, ttl=ttl) / UDP(dport=33434)
-    # Envia los paquetes y obtiene un reply
+    #El puerto 33434 es el traceroute para la localizacion de rutas.
+    # Envia los paquetes y obtiene un reply (sr1() es una funcion que envia un paquete y escucha la respuesta, en este caso si no se recibe respuesta salta el "break")
     reply = sr1(pkt, verbose=0)
     if reply is None:
         # Si no recibe el reply se para
+	
         break
     elif reply.type == 3:
         # Llega con exito
