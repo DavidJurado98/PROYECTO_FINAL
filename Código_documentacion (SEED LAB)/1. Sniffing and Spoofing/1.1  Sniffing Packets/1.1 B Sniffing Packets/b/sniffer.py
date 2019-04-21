754096d6e818feaaff8e561865559bdcd8eb96a0
DavidJurado98 <33413477+DavidJurado98@users.noreply.github.com>
2019-04-21 06:24:33 +0200

    CODIGO PROYECTO

--- Código_documentacion (SEED LAB)/1. Sniffing and Spoofing/1.1  Sniffing Packets/1.1 B Sniffing Packets/b/sniffer.py
+++ Código_documentacion (SEED LAB)/1. Sniffing and Spoofing/1.1  Sniffing Packets/1.1 B Sniffing Packets/b/sniffer.py
@@ -3,5 +3,5 @@
 from scapy.all import *
 def print_pkt(pkt):
 	pkt.show()
-pkt = sniff(iface="enp0s3",filter="tcp and host 192.168.1.120 and port 80",\
+pkt = sniff(iface="enp0s3",filter="tcp and host 192.168.1.120 and port 23",\
 prn = lambda x: x.summary("%IP.src% %IP.dst% %IP.proto%"))
