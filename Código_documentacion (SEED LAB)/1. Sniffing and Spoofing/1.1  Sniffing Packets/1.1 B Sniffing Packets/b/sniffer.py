15ad04ee932c0a7de8b80beb2544f802113dc0a1
DavidJurado98 <33413477+DavidJurado98@users.noreply.github.com>
2019-04-21 07:56:46 +0200

    CODIGO PROYECTO

--- Código_documentacion (SEED LAB)/1. Sniffing and Spoofing/1.1  Sniffing Packets/1.1 B Sniffing Packets/b/sniffer.py
+++ Código_documentacion (SEED LAB)/1. Sniffing and Spoofing/1.1  Sniffing Packets/1.1 B Sniffing Packets/b/sniffer.py
@@ -3,5 +3,5 @@
 from scapy.all import *
 def pkt_callback(pkt):
     pkt.show() # debug statement
-sniff(iface="enp0s3", prn=pkt_callback, filter="tcp", store=0)
+sniff(iface="enp0s3", prn=pkt_callback, filter="tcp and host 192.168.1.120 and port 23", store=0)
 
