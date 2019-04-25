923bda6da4acc912488ffe0253acbb8d7a62249d
DavidJurado98 <33413477+DavidJurado98@users.noreply.github.com>
2019-04-25 11:35:10 +0200

    CODIGO PROYECTO

--- Código_documentacion (SEED LAB)/1. Sniffing and Spoofing/1.1  Sniffing Packets/1.1 B Sniffing Packets/sniffer(a,b,c).py
+++ Código_documentacion (SEED LAB)/1. Sniffing and Spoofing/1.1  Sniffing Packets/1.1 B Sniffing Packets/sniffer(a,b,c).py
@@ -6,11 +6,13 @@ from scapy.all import *
 def print_pkt(pkt):
 	pkt.show()
 
+#Elimine el simbolo "#" de la linea del ejercicio que quiera realizar
+	
 #EJERCICIO A (Filtrar por paquetes ICMP)
-pkt = sniff(filter='icmp', prn=print_pkt)
+#pkt = sniff(filter='icmp', prn=print_pkt)
 
 #EJERCICIO B (Filtrar por paquetes TCP y puerto 23)
 #pkt = sniff(filter='tcp port 23', prn=print_pkt)
 
 #EJERCICIO C (Filtrar por subred)
-#pkt = sniff(filter='net 8.8.8.0/24', prn=print_pkt)
+#pkt = sniff(filter='net 128.230.0.0/16', prn=print_pkt)
